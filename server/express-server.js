ba1d73f3cbd8716e2433533bd37b245c23161324
Nathan Iszlaub <NathanIszlaub@gmail.com>
2021-03-30 23:48:00 +0800

    Setup simple `/items` endpoint

--- server/express-server.js
+++ server/express-server.js
@@ -57,6 +57,56 @@ const app = express();
 const port = argv.port ? argv.port : 3000;
 
 app.use("/", express.static("./public/"));
+app.use(express.json());
+
+function ensureProp(value, name, res) {
+	if (value === undefined)
+		res.status(422).send(`Missing required property \`${name}\``);
+	return value !== undefined;
+}
+
+app.put("/items/create", (req, res) => {
+	dbClient
+		.query(
+			'CREATE TABLE "items" (' +
+				'"id"    CHAR(36),' +
+				'"value" VARCHAR(80),' +
+				'"done"  BOOLEAN,' +
+				'PRIMARY KEY ("id"))'
+		)
+		.then(() => res.status(200).send("Successfully initialised `items`"))
+		.catch((err) => res.send(err.message));
+});
+
+app.put("/items/drop", (req, res) => {
+	dbClient
+		.query('DROP TABLE "items"')
+		.then(() => res.status(200).send("Successfully destroyed `items`"))
+		.catch((err) => res.send(err.message));
+});
+
+app.put("/items", (req, res) => {
+	if (
+		ensureProp(req.body.id, "id", res) &&
+		ensureProp(req.body.value, "value", res) &&
+		ensureProp(req.body.done, "done", res)
+	) {
+		dbClient
+			.query(
+				'INSERT INTO "items" VALUES ($1, $2, $3) ON CONFLICT ("id") DO UPDATE SET "value" = $2, "done" = $3',
+				[req.body.id, req.body.value, req.body.done]
+			)
+			.then(() => res.status(200).send("Successfully updated item"))
+			.catch((err) => res.send(err.message));
+	}
+});
+
+app.get("/items", (req, res) => {
+	dbClient
+		.query('SELECT * FROM "items"')
+		.then((dbRes) => res.status(200).send(dbRes.rows))
+		.catch((err) => res.send(err.message));
+});
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
