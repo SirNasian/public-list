ee7d34fb7548eca839824d5ef23ecc7d43dd40e4
Nathan Iszlaub <NathanIszlaub@gmail.com>
2021-04-02 21:47:34 +0800

    Implement item update requests

--- src/CustomList.tsx
+++ src/CustomList.tsx
@@ -15,6 +15,16 @@ type CustomListItem = {
 	done: boolean;
 };
 
+function updateItem(item: CustomListItem) {
+	window
+		.fetch(`http://localhost:3000/items`, {
+			method: "post",
+			body: JSON.stringify(item),
+			headers: { "Content-Type": "application/json" },
+		})
+		.catch((err) => console.log(err));
+}
+
 export const CustomList: React.FC<CustomListProps> = ({
 	code,
 }: CustomListProps): JSX.Element => {
@@ -22,20 +32,21 @@ export const CustomList: React.FC<CustomListProps> = ({
 	const [items, setItems] = React.useState<CustomListItem[]>([]);
 
 	React.useEffect(() => {
-		window.fetch(`http://localhost:3000/items/${code}`)
+		window
+			.fetch(`http://localhost:3000/items/${code}`)
 			.then((res) => res.json())
 			.then((json) => setItems(json))
 			.catch((err) => console.log(err));
 	}, []);
 
 	const handleToggleDone = (id: string): void => {
-		setItems(
-			items.map((item) => {
-				const newItem = { ...item };
-				if (newItem.id === id) newItem.done = !newItem.done;
-				return newItem;
-			})
-		);
+		const newItems = items.map((item) => {
+			const newItem = { ...item };
+			if (newItem.id === id) newItem.done = !newItem.done;
+			return newItem;
+		});
+		setItems(newItems);
+		updateItem(newItems.find((item) => item.id === id));
 	};
 
 	const handleValueChange = (id: string, value: string): void => {
@@ -49,22 +60,22 @@ export const CustomList: React.FC<CustomListProps> = ({
 	};
 
 	const handleItemBlur = (id: string, value: string): void => {
-		if (!value)
-			setItems(items.filter(item => item.id !== id));
+		if (!value) setItems(items.filter((item) => item.id !== id));
+		else updateItem(items.find((item) => item.id === id));
 	};
 
 	const handleNewItem = (value: string): void => {
-		if (value)
-			setItems([
-				...items,
-				{
-					id: uuidv4(),
-					code: code,
-					value: value,
-					done: false,
-				},
-			]);
-		setNewItemValue("");
+		if (value) {
+			const newItem: CustomListItem = {
+				id: uuidv4(),
+				code: code,
+				value: value,
+				done: false,
+			};
+			setItems([...items, newItem]);
+			setNewItemValue("");
+			updateItem(newItem);
+		}
 	};
 
 	return (
@@ -83,20 +94,26 @@ export const CustomList: React.FC<CustomListProps> = ({
 									color="primary"
 									onChange={() => handleToggleDone(item.id)}
 								/>
-							)
+							),
 						}}
-						onBlur={event => handleItemBlur(item.id, event.target.value)}
-						onChange={event => handleValueChange(item.id, event.target.value)}
-						onKeyPress={event => event.key === 'Enter' ? (event.target as HTMLElement).blur() : null}
+						onBlur={(event) => handleItemBlur(item.id, event.target.value)}
+						onChange={(event) => handleValueChange(item.id, event.target.value)}
+						onKeyPress={(event) =>
+							event.key === "Enter"
+								? (event.target as HTMLElement).blur()
+								: null
+						}
 						value={item.value}
 					/>
 				</Box>
 			))}
 			<TextField
 				fullWidth
-				onBlur={event => handleNewItem(event.target.value)}
-				onChange={event => setNewItemValue(event.target.value)}
-				onKeyPress={event => event.key === 'Enter' ? handleNewItem(newItemValue) : null}
+				onBlur={(event) => handleNewItem(event.target.value)}
+				onChange={(event) => setNewItemValue(event.target.value)}
+				onKeyPress={(event) =>
+					event.key === "Enter" ? handleNewItem(newItemValue) : null
+				}
 				value={newItemValue}
 			/>
 		</>
